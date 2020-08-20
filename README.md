5f5b5858bb19e34769f682d0b0cb4d5b953255b5
Jonas Gertz <Geertzo@live.se>
2020-08-20 12:52:21 +0200

    added CR for frontend

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # nackademin-todo-app
 
 # Packages:
-# express, path, dotenv, cors, mongoose
+# express, path, dotenv, cors, mongoose, nodemon
+
+# Quasar is based on vue
+
+# cd backend -> nodemon app.js (port 8081)
+# cd frontend -> quasar dev (port 8080)
+
+# Jonas Gertz
--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const todoRoute = require('./routes/todo');
 const app = express();
-
+var cors = require('cors')
 // const frondEnd = require('./routes/frontend');
 
 const path = require('path')
@@ -13,6 +13,7 @@ const port = process.env.PORT || 8081;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('./public'))
+app.use(cors())
 
 /**
  * Route link
